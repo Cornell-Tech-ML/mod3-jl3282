1e6de6a81ff2da4a1e0ba668751db4bf60488706
Jinny Lee <jl3282@cornell.edu>
2024-11-16 21:19:36 -0500

    cuda_3_5

--- project/run_fast_tensor.py
+++ project/run_fast_tensor.py
@@ -30,8 +30,10 @@ class Network(minitorch.Module):
 
     def forward(self, x):
         # TODO: Implement for Task 3.5.
-        raise NotImplementedError("Need to implement for Task 3.5")
-
+        h = self.layer1.forward(x).relu()
+        h = self.layer2.forward(h).relu()
+        return self.layer3.forward(h).sigmoid()
+    
 
 class Linear(minitorch.Module):
     def __init__(self, in_size, out_size, backend):
@@ -44,7 +46,14 @@ class Linear(minitorch.Module):
 
     def forward(self, x):
         # TODO: Implement for Task 3.5.
-        raise NotImplementedError("Need to implement for Task 3.5")
+        if len(x.shape) == 1:
+            x = x.view(1, x.shape[0])
+            
+        out = x @ self.weights.value
+        
+        return out + self.bias.value
+
+        
 
 
 class FastTrain:
